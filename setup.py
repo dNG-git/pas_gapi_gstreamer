732b9fa496dfa50599709a9fb5726ca20bfef727
NotTheEvilOne <git-NTEO@vplace.de>
2016-05-13 11:58:44 +0200

    Trivial: Added "platforms" list to "setup.py" Trivial: Added "et al." to the authors attribute in "setup.py"

--- setup.py
+++ setup.py
@@ -65,10 +65,12 @@ with TemporaryDirectory(dir = ".") as build_directory:
 	      version = get_version(),
 	      description = "Python Application Services",
 	      long_description = """"pas_gapi_gstreamer" is an adapter and abstraction layer for the C-Level GStreamer API.""",
-	      author = "direct Netware Group",
+	      author = "direct Netware Group et al.",
 	      author_email = "web@direct-netware.de",
 	      license = "GPLv2+",
 	      url = "https://www.direct-netware.de/redirect?pas;gapi;gstreamer",
+
+	      platforms = [ "any" ],
 
 	      package_dir = { "": _build_path },
 	      packages = [ "dNG" ],
